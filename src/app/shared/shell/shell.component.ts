4adba95bfbadf153f6249a49582599bf67e27a86
unknown <jason.sigler@outlook.com>
2020-01-02 18:20:47 -0500

    Initial Commit

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver} from '@angular/cdk/layout';
+import { Observable} from 'rxjs';
+
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
