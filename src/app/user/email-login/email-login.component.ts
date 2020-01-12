294daa6b69d56d577df13d9b49a9e71043efd4ad
Jason Sigler <jason.sigler@outlook.com>
2020-01-12 01:22:49 -0500

    Fixed backend Data Models
    Fixed the backend data models and fine-tuned the scss

--- src/app/user/email-login/email-login.component.ts
+++ src/app/user/email-login/email-login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/auth";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
   selector: "app-email-login",
@@ -9,17 +9,19 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class EmailLoginComponent implements OnInit {
   form: FormGroup;
+
   type: "login" | "signup" | "reset" = "signup";
-  serverMessage: string;
   loading = false;
+
+  serverMessage: string;
 
   constructor(private afAuth: AngularFireAuth, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.fb.group({
       email: ["", [Validators.required, Validators.email]],
-      password: ["", [Validators.required, Validators.minLength(6)]],
-      passwordConfirmation: ["", []]
+      password: ["", [Validators.minLength(6), Validators.required]],
+      passwordConfirm: ["", []]
     });
   }
 
@@ -42,15 +44,12 @@ export class EmailLoginComponent implements OnInit {
   get email() {
     return this.form.get("email");
   }
-
   get password() {
     return this.form.get("password");
   }
-
   get passwordConfirm() {
     return this.form.get("passwordConfirm");
   }
-
   get passwordDoesMatch() {
     if (this.type !== "signup") {
       return true;
@@ -58,11 +57,12 @@ export class EmailLoginComponent implements OnInit {
       return this.password.value === this.passwordConfirm.value;
     }
   }
-
   async onSubmit() {
     this.loading = true;
+
     const email = this.email.value;
     const password = this.password.value;
+
     try {
       if (this.isLogin) {
         await this.afAuth.auth.signInWithEmailAndPassword(email, password);
@@ -72,10 +72,12 @@ export class EmailLoginComponent implements OnInit {
       }
       if (this.isPasswordReset) {
         await this.afAuth.auth.sendPasswordResetEmail(email);
-        this.serverMessage = "Email Sent - Check Your Inbox";
+        this.serverMessage = "Check your email";
       }
-    } catch (e) {
-      this.serverMessage = e;
+    } catch (err) {
+      this.serverMessage = err;
     }
+
+    this.loading = false;
   }
 }
